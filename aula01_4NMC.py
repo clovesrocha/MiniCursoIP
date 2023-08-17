fb5cdd81951dca6033454cd13d900b68e6cbfb41
Guilherme Freitas <99972010+guifreitasds@users.noreply.github.com>
2023-08-17 09:24:32 -0300

    Add files via upload

---
+++ aula01_4NMC.py
@@ -0,0 +1,16 @@
+# Aula - Ep. #02 - Mini Curso de #IP
+# Tipos Primitivos de Dados em Python
+# Docente: Prof MsC Cloves Rocha
+# Estudantes: Ellen, Guilherme, Maria Luiza, Nicolas e Geraldo
+# Turma: 4 NMC
+
+# Tipo Inteiro ok
+# Tipo Reais ok
+# Tipo Booleanos ok
+# Tipo Caracteres (string) ok
+
+c = "Pendrive"
+c = 5
+print (type(c))
+r = 5
+print(c == r)
