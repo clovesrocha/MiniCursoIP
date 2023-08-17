2498cb79287c6c28463c2b395a5060bc8c9a9e0c
Mateus Rebêlo Fernandes <88837654+mateusrf@users.noreply.github.com>
2023-08-17 10:13:58 -0300

    Aula 02 - Code - Mateus

---
+++ aula2.py
@@ -0,0 +1,16 @@
+#uninassau
+#alunos: Mateus Rebelo Fernandes - 01559114 / Caio Henrique - 01558106 / Gabriel cruz pereira - 01514331
+#tipo inteiro, real, booleanos, caracters(str)
+
+
+c=(5)
+print(type(c))
+
+a=("mateusinho") 
+print(type(a))
+
+b=(True) 
+print(type(b))
+
+d=(2.3)
+print(type(d))
