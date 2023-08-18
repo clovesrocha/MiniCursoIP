5fbc13f57cfe628024c63de8d8a4bc86371a99cd
profclovesrocha <clovesoficial@gmail.com>
2023-08-18 11:52:38 -0300

    Demo 2 - SI/EC/CC 2MC

---
+++ aula00.py
@@ -0,0 +1,60 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPeZd+YS5S5lhpZGmMI+3lq",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/clovesrocha/MiniCursoIP/blob/master/aula00.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 2,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "mkvVOJqw7Njf",
+        "outputId": "27ce3a51-21ea-4207-a75e-d1c81dd61b8d"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "<class 'bool'>\n"
+          ]
+        }
+      ],
+      "source": [
+        "# ESCREVA AQUI SEU COMENTÁRIO...\n",
+        "# UNINASSAU\n",
+        "# DOCENTE: PROF. MSc Cloves\n",
+        "# ESTUDANTE:\n",
+        "\n",
+        "#print('Olá mundo!')\n",
+        "c = True\n",
+        "print(type(c))"
+      ]
+    }
+  ]
+}
