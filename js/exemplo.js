bbd45a195d5cac98f28ae0ea2ebc47a7c5ea3154
profclovesrocha <clovesoficial@gmail.com>
2023-09-28 15:57:47 -0300

    Escrevendo uma função em JS

---
+++ js/exemplo.js
@@ -0,0 +1,11 @@
+var capitulo = 'Scope JS';
+
+function novoNome() {
+  var nome = 'Rocha';
+  var email = 'exemplo@gmail.com';
+  console.log(capitulo);
+  console.log(nome);
+  console.log(email);
+}
+
+novoNome();
