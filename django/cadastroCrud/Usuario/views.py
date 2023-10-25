b0278869435be65777ecad473cf877251f4f9681
Helton França <144186013+HeltonFranca@users.noreply.github.com>
2023-10-25 00:30:02 -0300

    Update views.py

--- django/cadastroCrud/Usuario/views.py
+++ django/cadastroCrud/Usuario/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render, redirect, get_object_or_404  
-from .models import Usuario  
-from django.urls import reverse  
-from django.core import serializers 
-from .serializers import UsuarioSerializer,UsuarioUpdateSerializer 
-from rest_framework.renderers import TemplateHTMLRenderer 
-from rest_framework.response import Response 
-from rest_framework import status, generics  
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Usuario
+from django.urls import reverse
+from django.core import serializers
+from .serializers import UsuarioSerializer,UsuarioUpdateSerializer
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework import status, generics
 
-class CadastrarUsuario(generics.CreateAPIView):
+
+class cadastar_usuario(generics.CreateAPIView):
     serializer_class = UsuarioSerializer
     def get(self, request, *args, **kwargs):
        
@@ -15,6 +16,7 @@ class CadastrarUsuario(generics.CreateAPIView):
        
         return render(request, 'cadastrousuario.html', {'serializer': serializer})
 
+    
     def post(self, request, *args, **kwargs):
         
         serializer = self.serializer_class(data=request.data)
@@ -24,25 +26,26 @@ class CadastrarUsuario(generics.CreateAPIView):
             serializer.save()
             
             return redirect('cadastro')
-
+        else:
+            return Response({'serializer': serializer}, status=status.HTTP_400_BAD_REQUEST)
+        
 class ListarUsuario(generics.ListCreateAPIView):
-    queryset = Usuario.objects.all()
+    queryset = Usuario.objects.all
     serializer_class = UsuarioSerializer
 
-    def get(self, request, *args, **kwargs):
-        
+    def get(self,request,*args,**kwargs):
         usuarios = self.get_queryset()
-
-        return render(request,'listarusuario.html', {'usuarios':usuarios})
+        return render(request,'listarusuario.html',{'usuarios':usuarios})
     
-    def post(self, request, *args, **kwargs):
+    def post(self,request,*args,**kwargs):
 
         serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
-            return redirect('listar')
-    
-
+            return redirect('cadastro')
+        else:
+            return render(request, 'listarusuario.html', {'usuarios': self.get_queryset(), 'serializer': serializer})
+        
 class UpdateUsuario(generics.UpdateAPIView):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
@@ -52,7 +55,7 @@ class UpdateUsuario(generics.UpdateAPIView):
         user_id = kwargs.get('pk')
         usuario = get_object_or_404(Usuario, pk=user_id)
         serializer = self.serializer_class(instance=usuario)
-        return render(request, 'atualizarusuario.html', {'serializer': serializer, 'usuario': usuario})
+        return render(request, 'atualizar_usuario.html', {'serializer': serializer, 'usuario': usuario})
     
     def post(self, request, *args, **kwargs):
         user_id = kwargs.get('pk')
@@ -64,27 +67,37 @@ class UpdateUsuario(generics.UpdateAPIView):
         if serializer.is_valid():
             serializer.save()
             return redirect('listar') 
-        return render(request, 'atualizarusuario.html', {'serializer': serializer, 'usuario': usuario})
-    
+        return render(request, 'atualizar_usuario.html', {'serializer': serializer, 'usuario': usuario})
 
 class DeleteUsuario(generics.DestroyAPIView):
 
     queryset = Usuario.objects.all()
+    
+ 
     serializer_class = UsuarioSerializer
 
 
     def get(self, request, *args, **kwargs):
 
         user_id = kwargs.get('pk')
-        usuario = get_object_or_404(Usuario,pk=user_id)
+     
+        usuario = get_object_or_404(Usuario, pk=user_id)
+    
         serializer = self.serializer_class(instance=usuario)
-    
-        return render(request, 'listarusuario.html',{'serializer':serializer,'usuario':usuario})
-    
+
+        return render(request, 'listarusuario.html', {'serializer': serializer, 'usuario': usuario})
+
+   
     def post(self, request, *args, **kwargs):
-
+        
         user_id = kwargs.get('pk')
-        usuario = get_object_or_404(Usuario,pk=user_id)
-
+       
+        usuario = get_object_or_404(Usuario, pk=user_id)
+        
         usuario.delete()
+        
         return redirect('listar')
+
+
+
+
